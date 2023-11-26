9b0d1ec6355c7157c4ec5ab035ea03fc100ec24f
bytzo <bytzo@bytzo.net>
2023-11-26 02:10:12 -0800

    Cancel setHrtf method HRTF status is already set as an attribute when creating the audio context, so it is now unnecessary to set it before

--- src/main/java/net/bytzo/mc_265514/mixins/LibraryMixin.java
+++ src/main/java/net/bytzo/mc_265514/mixins/LibraryMixin.java
@@ -10,7 +10,9 @@ import org.lwjgl.openal.SOFTHRTF;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.nio.IntBuffer;
 
@@ -52,5 +54,10 @@ public class LibraryMixin {
 		}
 
 		return attributes.put(originalPut);
+	}
+
+	@Inject(method = "setHrtf(Z)V", at = @At("HEAD"), cancellable = true)
+	private void cancelSetHrtf(boolean setHrtf, CallbackInfo callbackInfo) {
+		callbackInfo.cancel();
 	}
 }
